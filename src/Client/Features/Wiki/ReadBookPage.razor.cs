413eec36ed46a91acda21bbb181aedf98275d215
MindHardt <igorbabin2002@gmail.com>
2024-04-30 23:48:38 +0500

    Added books image disabling

---
+++ src/Client/Features/Wiki/ReadBookPage.razor.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Client.Features.Wiki;
+
+public partial class ReadBookPage
+{
+    private string GetText() => _includeImages
+        ? _book!.Text
+        : ExcludeImageRegex().Replace(_book!.Text, string.Empty);
+    
+    [GeneratedRegex(@"!\[.*?\]\(.*?\)")]
+    private static partial Regex ExcludeImageRegex();
+    
+    
+}
