e642037a459cb440d99ae7650bf4521650466abc
MindHardt <igorbabin2002@gmail.com>
2024-05-27 21:37:24 +0300

    Added value converter file from previous commit

---
+++ src/Server/Data/Alids/AlidEntityValueConverter.cs
@@ -0,0 +1,34 @@
+﻿using Arklens.Alids;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+namespace Server.Data.Alids;
+
+internal class AlidEntityValueConverter<TEntity> : ValueConverter<TEntity, string>
+    where TEntity : IAlidEntity
+{
+    public AlidEntityValueConverter() : base(
+        entity => entity.Alid.Value,
+        alid => IAlidEntity.Get<TEntity>(Alid.Create(alid)))
+    { }
+}
+
+internal static class AlidEntityValueConverterExtensions
+{
+    public static ModelConfigurationBuilder ConfigureAlidEntityConversions(this ModelConfigurationBuilder model)
+    {
+        var alidTypes = typeof(Alid).Assembly
+            .GetTypes()
+            .Where(x => x.IsAssignableTo(typeof(IAlidEntity)));
+        foreach (var alidType in alidTypes)
+        {
+            var converterType = typeof(AlidEntityValueConverter<>).MakeGenericType(alidType);
+            model.Properties(alidType)
+                .HaveConversion(converterType)
+                .HaveMaxLength(Alid.MaxLength)
+                .UseCollation("C");
+        }
+
+        return model;
+    }
+}
