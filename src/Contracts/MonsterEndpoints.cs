8cbc25e406b5f5badf3a0bcd18bb3ad3672a20cc
MindHardt <igorbabin2002@gmail.com>
2024-06-23 01:28:53 +0500

    feature/monster-wiki: Added basic monster model

---
+++ src/Contracts/MonsterEndpoints.cs
@@ -0,0 +1,15 @@
+﻿using Arklens;
+
+namespace Contracts;
+
+public static class MonsterEndpoints
+{
+    public const string Path = "monsters";
+    public const string FullPath = $"{WikiEndpoints.FullPath}/{Path}";
+}
+
+public record MonsterModel(
+    string Name,
+    Alignment Alignment,
+    FileIdentifier CoverImage,
+    FileIdentifier MinifigureImage);
