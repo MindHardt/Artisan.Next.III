e2acc4c10f4b8456f38011b3980eaf9c7a2d2364
igorb <igorbabin2002@gmail.com>
2024-08-01 12:23:47 +0500

    Improved link UI for rendered markdown

--- src/Client/Features/Shared/Components/RenderedMd.razor.js
+++ src/Client/Features/Shared/Components/RenderedMd.razor.js
@@ -1,14 +1,20 @@
+// noinspection SpellCheckingInspection
 const createConverter = () => new showdown.Converter({
     emoji: true,
     parseImgDimensions: true,
     strikethrough: true,
-    simplifiedAutoLink: true
+    simplifiedAutoLink: true,
+    tasklists: true,
 });
 
 export function renderMd(md) {
-    return createConverter().makeHtml(md);
+    const html = createConverter().makeHtml(md);
+    const result = document.createElement('div');
+    result.innerHTML = html;
+    result.querySelectorAll('a').forEach(a => a.className = 'text-decoration-none text-link');
+    return result.innerHTML;
 }
 
 export function renderTo(md, elementId) {
-    document.getElementById(elementId).innerHTML = createConverter().makeHtml(md);
+    document.getElementById(elementId).innerHTML = renderMd(md);
 }
