ebcac6d57571402116a405c9eb50367e2faaf864
igorb <igorbabin2002@gmail.com>
2024-08-02 02:27:27 +0500

    Made themes persistent

---
+++ src/Client/wwwroot/js/theme.local.js
@@ -0,0 +1,30 @@
+function getTheme() {
+    return localStorage.getItem('theme') ?? 'light';
+}
+function fixThemeIcon() {
+    const themeIcon = document.getElementById('themeIcon');
+    if (themeIcon === null) {
+        return;
+    }
+    const theme = getTheme();
+    const icon = theme === 'light' ? 'bi-sun-fill' : 'bi-moon-fill';
+    themeIcon.className = 'text-warning bi ' + icon;
+}
+function setTheme(theme) {
+    document.body.getAttributeNode('data-bs-theme').value = theme;
+    localStorage.setItem('theme', theme);
+    fixThemeIcon();
+    console.log('Theme set to ', theme)
+}
+function changeTheme() {
+    const currentTheme = getTheme();
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+}
+function restoreTheme() {
+    const theme = getTheme();
+    const themeAttr = document.body.getAttributeNode('data-bs-theme') ?? document.createAttribute('data-bs-theme');
+    document.body.attributes.setNamedItem(themeAttr);
+    setTheme(theme);
+}
+restoreTheme();
